c84d4b406ac70cf2767260d75ed32b014f9b32f7
Dolon Chandra Roy <dolonr718@gmail.com>
2024-05-20 20:44:23 +0600

    mongoose project setup starting commit

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import { Application, Request, Response } from "express";
+import express from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  const a = 10;
+
+  console.log(a, "something-new");
+
+  res.send("Hello World!");
+});
+
+export default app;
